b28bd3f3085213e4d06be98f07c380d1f3e4ac85
Phase1es2 <hyang17@uw.edu>
2025-09-04 12:13:46 -0400

    Assignment 1&2 format

--- C Programming Language/Assignment 2/isleapyear.c
+++ C Programming Language/Assignment 2/isleapyear.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int isLeapYear(int x) {
+// return 1 if it is leap yaer, 0 not
+int isLeapYear(int x)
+{
+    // leap year if it is divisible by 4 but not by 100 unless it is also divisible by 400
     if ((!(x % 4) && (x % 100)) || !(x % 400)) return 1;
     else return 0;
 }
@@ -12,14 +15,12 @@ int main(int argc, char *argv[]) {
         printf("Argument count %s <number>\n", argv[0]);
         return 1;
     }
-    // int atoi(const cahr *str) 
+    // int atoi(const cahr *str)
     // convert string to integer
     int x = atoi(argv[1]);
 
     if (isLeapYear(x) == 1) printf("{%d} was a leap year\n", x);
     else printf("{%d} was not a leap year\n", x);
 
-
-    
     return 0;
 }
