17351a9ab3a8bbd8a7aeec42e4787763cade588b
Phase1es2 <hyang17@uw.edu>
2025-09-15 00:45:32 -0400

    complete cpu.c

--- CPU Scheduling/cpu.c
+++ CPU Scheduling/cpu.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "oslabs.h"
+#include "../Disk Scheduling/oslabs.h"
 #define PID process_id
 #define AT arrival_timestamp
 #define TBT total_bursttime
@@ -8,35 +8,209 @@
 #define RBT remaining_bursttime
 #define PP process_priority
 
-//NULLPCB is defined as [PID:0, AT:0, TBT:0, EST:0, EET:0, RBT:0, Priority:0]
-struct PCB NULLPCB = {[0, 0, 0, 0, 0, 0, 0]};
+// NULLPCB is defined as [PID:0, AT:0, TBT:0, EST:0, EET:0, RBT:0, Priority:0]
+struct PCB NULLPCB = {0, 0, 0, 0, 0, 0, 0};
 
+// helper to compare if an inpcb is NULLPCB
 int check_exited_process(struct PCB inpcb) {
   if (inpcb.PID == NULLPCB.PID && inpcb.AT == NULLPCB.AT &&
       inpcb.TBT == NULLPCB.TBT && inpcb.EST == NULLPCB.EST &&
       inpcb.EET == NULLPCB.EET && inpcb.RBT == NULLPCB.RBT &&
       inpcb.PP == NULLPCB.PP)
-    í return 1;
+    return 1;
 
   return 0;
 }
 
 struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX],
                                      int* queue_cnt, struct PCB current_process,
-                                     struct PCB new_process, int timestamp) {}
+                                     struct PCB new_process, int timestamp) {
+  if (check_exited_process(current_process)) {
+    // the new process is modified so that the execution start time is set to the current timestamp
+    new_process.EST = timestamp;
+    // execution end time is set to the sum of the current timestamp and total burst time
+    new_process.EET = timestamp + new_process.TBT;
+    // the remaining burst time is set to the total burst time
+    new_process.RBT = new_process.TBT;
+    return new_process;
+  } else if (current_process.PP <= new_process.PP) {
+    // its execution start time and execution end time are set to 0
+    new_process.EST = 0;
+    new_process.EET = 0;
+    // the remaining burst time is the same as its total burst time.
+    new_process.RBT = new_process.TBT;
+    // added to the ready queue
+    ready_queue[(*queue_cnt)++] = new_process;
+    // return value is the PCB of the currently-running process
+    return current_process;
+  } else {
+    // new process is modified so that the execution start time is set to the current timestamp
+    new_process.EST = timestamp;
+    // execution end time is set to the sum of the current timestamp and the total burst time
+    new_process.EET = timestamp + new_process.TBT;
+    // remaining burst time is set to the total burst time
+    new_process.RBT = new_process.TBT;
+    // currently-running process is added to the ready queue
+
+    // the PCB of the currently-running process is added to the ready queue
+    // after marking its execution end time as 0, and adjusting its remaining burst time.
+    current_process.EET = 0;
+    current_process.RBT -= timestamp - current_process.EST;
+    ready_queue[(*queue_cnt)++] = current_process;
+    // return value is the PCB of the new process
+    return new_process;
+  }
+}
+
 struct PCB handle_process_completion_pp(struct PCB ready_queue[QUEUEMAX],
-                                        int* queue_cnt, int timestamp) {}
+                                        int* queue_cnt, int timestamp) {
+  // queue is empty, the method returns the NULLPCB
+  if (!(*queue_cnt))
+    return NULLPCB;
+
+  // idx for returned_pcb
+  int idx = 0;
+  // finds the PCB of the process in the ready queue with the highest priority
+  for (int i = 0; i < (*queue_cnt); i++)
+    if (ready_queue[i].PP < ready_queue[idx].PP)
+      idx = i;
+
+  struct PCB returned_pcb = ready_queue[idx];
+  // removes this PCB from the ready queue
+  for (int i = idx; i + 1 < (*queue_cnt); i++)
+    ready_queue[i] = ready_queue[i + 1];
+  --(*queue_cnt);
+
+  // execution start time as the current timestamp and the execution end time
+  // as the sum of the current timestamp and the remaining burst time
+  returned_pcb.EST = timestamp;
+  returned_pcb.EET = timestamp + returned_pcb.RBT;
+
+  return returned_pcb;
+}
 struct PCB handle_process_arrival_srtp(struct PCB ready_queue[QUEUEMAX],
                                        int* queue_cnt,
                                        struct PCB current_process,
                                        struct PCB new_process, int time_stamp) {
+  // If there is no currently-running process
+  if (check_exited_process(current_process)) {
+    // execution start time is set to the current timestamp
+    new_process.EST = time_stamp;
+    // execution end time is set to the sum of the current timestamp
+    new_process.EET = time_stamp + new_process.TBT;
+    // the remaining burst time is set to the total burst time
+    new_process.RBT = new_process.TBT;
+    // returns the PCB of the newly-arriving process
+    return new_process;
+    // If tcurrently-running process
+    // compares the remaining burst time of the currently-running process
+    // and the total burst time of the newly-arriving process
+  } else if (current_process.RBT <= new_process.TBT) {
+    // then its PCB is simply added to the ready queue
+    new_process.EST = 0;
+    new_process.EET = 0;
+    new_process.RBT = new_process.TBT;
+    ready_queue[(*queue_cnt)++] = new_process;
+    // return value is the PCB of the currently running process
+    return current_process;
+  } else {  // If the new process has a shorter burst time
+    // the new process: the execution start time is set to the current timestamp
+    new_process.EST = time_stamp;
+    // execution end time is set to the sum of the current timestamp and the total burst time
+    new_process.EET = time_stamp + new_process.TBT;
+    // the remaining burst time is set to the total burst time
+    new_process.RBT = new_process.TBT;
+    // PCB of the currently-running process is added to the ready queue
+    current_process.RBT -= time_stamp - current_process.EST;
+    current_process.EST = 0;
+    current_process.EET = 0;
+    ready_queue[(*queue_cnt)++] = current_process;
+
+    // return value is the PCB of the new process
+    return new_process;
+  }
 }
 struct PCB handle_process_completion_srtp(struct PCB ready_queue[QUEUEMAX],
-                                          int* queue_cnt, int timestamp);
+                                          int* queue_cnt, int timestamp) {
+  // queue is empty, the method returns the NULLPCB
+  if (!(*queue_cnt))
+    return NULLPCB;
+
+  int idx = 0;
+  // finds the process with the smallest remaining burst time
+  for (int i = 0; i < (*queue_cnt); i++)
+    if (ready_queue[i].RBT < ready_queue[idx].RBT)
+      idx = i;
+  // return pcb
+  struct PCB returned_pcb = ready_queue[idx];
+
+  // removes this PCB
+  for (int i = idx; i + 1 < (*queue_cnt); i++)
+    ready_queue[i] = ready_queue[i + 1];
+  --(*queue_cnt);
+
+  // the execution start time as the current timestamp
+  returned_pcb.EST = timestamp;
+  // the execution end time as the sum of the current timestamp and the remaining burst time.
+  returned_pcb.EET = timestamp + returned_pcb.RBT;
+
+  // return pcb with smallest reaming burst time
+  return returned_pcb;
+}
 struct PCB handle_process_arrival_rr(struct PCB ready_queue[QUEUEMAX],
                                      int* queue_cnt, struct PCB current_process,
                                      struct PCB new_process, int timestamp,
-                                     int time_quantum) {}
+                                     int time_quantum) {
+  // If there is no currently-running process
+  if (check_exited_process(current_process)) {
+    // returns the PCB of the newly-arriving process
+
+    // execution start time is set to the current timestamp
+    new_process.EST = timestamp;
+    // execution end time is set to the sum of the current timestamp
+    // and the smaller of the time quantum and the total burst time
+    new_process.EET = timestamp + MIN(time_quantum, new_process.TBT);
+    // remaining burst time is set to the total burst time.
+    new_process.RBT = new_process.TBT;
+    // returns the PCB of the newly-arriving process
+    return new_process;
+  } else {  // If there is a currently-running process
+    // execution start time and execution end time are set to 0
+    new_process.EST = 0;
+    new_process.EET = 0;
+    // the remaining burst time is set to the total burst time
+    new_process.RBT = new_process.TBT;
+    // adds the PCB of the newly-arriving process to the ready queue
+    ready_queue[(*queue_cnt)++] = new_process;
+
+    // return value is the PCB of the currently running process
+    return current_process;
+  }
+}
 struct PCB handle_process_completion_rr(struct PCB ready_queue[QUEUEMAX],
                                         int* queue_cnt, int timestamp,
-                                        int time_quantum) {}
+                                        int time_quantum) {
+  // the ready queue is empty, the method returns the NULLPCB
+  if (!(*queue_cnt))
+    return NULLPCB;
+  int idx = 0;
+  // finds the process with the earliest arrival time
+  for (int i = 0; i < (*queue_cnt); i++)
+    if (ready_queue[i].AT < ready_queue[idx].AT)
+      idx = i;
+
+  struct PCB returned_pcb = ready_queue[idx];
+
+  // removes this PCB from the ready queue
+  for (int i = idx; i + 1 < (*queue_cnt); i++)
+    ready_queue[i] = ready_queue[i + 1];
+  --(*queue_cnt);
+
+  // execution start time as the current timestamp
+  returned_pcb.EST = timestamp;
+  // execution end time as the sum of the current timestamp
+  // and the smaller of the time quantum and the remaining burst time
+  returned_pcb.EET = timestamp + MIN(returned_pcb.RBT, time_quantum);
+  // return PCB
+  return returned_pcb;
+}
---
+++ CPU Scheduling/main.c
@@ -0,0 +1,352 @@
+#include <stdio.h>
+#include <string.h>
+#include "../Disk Scheduling/oslabs.h"
+
+#define PID process_id
+#define AT arrival_timestamp
+#define TBT total_bursttime
+#define EST execution_starttime
+#define EET execution_endtime
+#define RBT remaining_bursttime
+#define PP process_priority
+
+void set_pcb(struct PCB* inpcb, int pid, int at, int tbt, int est, int eet,
+             int rbt, int pp) {
+  (*inpcb).PID = pid;
+  (*inpcb).AT = at;
+  (*inpcb).TBT = tbt;
+  (*inpcb).EST = est;
+  (*inpcb).EET = eet;
+  (*inpcb).RBT = rbt;
+  (*inpcb).PP = pp;
+}
+void print_pcb(const char* label, struct PCB r) {
+  printf("%s[PID: %d, AT: %d, TBT: %d, EST: %d, EET: %d, RBT: %d, PP: %d]\n",
+         label, r.PID, r.AT, r.TBT, r.EST, r.EET, r.RBT, r.PP);
+}
+
+void print_queue(const char* label, struct PCB queue[], int queue_cnt) {
+  if (queue_cnt == 0) {
+    printf("%sEmpty queue!\n", label);
+    return;
+  }
+  for (int i = 0; i < queue_cnt; i++) {
+    char entry[64];
+    snprintf(entry, sizeof(entry), "%s[%d] ", label, i);
+    print_pcb(entry, queue[i]);
+  }
+  printf("%squeue_cnt: %d\n", label, queue_cnt);
+}
+
+void handle_process_arrival_pp_test() {
+  struct PCB ready_queue[QUEUEMAX];
+  struct PCB b1, current_process, new_process, returned_pcb;
+  int queue_cnt = 0, timestamp = 2;
+  set_pcb(&current_process, 1, 1, 4, 1, 5, 4, 8);
+  set_pcb(&new_process, 2, 2, 3, 0, 0, 3, 6);
+
+  returned_pcb = handle_process_arrival_pp(
+      ready_queue, &queue_cnt, current_process, new_process, timestamp);
+
+  print_queue("Q1 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+
+  memset(ready_queue, 0, sizeof ready_queue);
+  queue_cnt = 0;
+  timestamp = 2;
+  set_pcb(&current_process, 0, 0, 0, 0, 0, 0, 0);
+  set_pcb(&new_process, 5, 2, 5, 0, 0, 5, 4);
+
+  returned_pcb = handle_process_arrival_pp(
+      ready_queue, &queue_cnt, current_process, new_process, timestamp);
+
+  print_queue("Q2 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+
+  memset(ready_queue, 0, sizeof ready_queue);
+  queue_cnt = 0;
+  timestamp = 4;
+
+  set_pcb(&b1, 6, 3, 4, 0, 0, 4, 5);
+  ready_queue[queue_cnt++] = b1;
+  set_pcb(&current_process, 5, 2, 5, 2, 7, 3, 4);
+  set_pcb(&new_process, 7, 4, 5, 0, 0, 5, 7);
+
+  returned_pcb = handle_process_arrival_pp(
+      ready_queue, &queue_cnt, current_process, new_process, timestamp);
+
+  print_queue("Q3 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+}
+
+void handle_process_completion_pp_test() {
+  struct PCB ready_queue[QUEUEMAX];
+  struct PCB b1, b2, b3, b4, returned_pcb;
+
+  int queue_cnt = 0;
+  int timestamp = 2;
+
+  set_pcb(&b1, 1, 1, 4, 0, 0, 4, 23);
+  set_pcb(&b2, 2, 1, 4, 0, 0, 4, 22);
+  set_pcb(&b3, 3, 1, 4, 0, 0, 4, 24);
+
+  ready_queue[queue_cnt++] = b1;
+  ready_queue[queue_cnt++] = b2;
+  ready_queue[queue_cnt++] = b3;
+
+  returned_pcb =
+      handle_process_completion_pp(ready_queue, &queue_cnt, timestamp);
+
+  print_queue("Q1 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+
+  memset(ready_queue, 0, sizeof ready_queue);
+  queue_cnt = 0;
+  timestamp = 3;
+  returned_pcb =
+      handle_process_completion_pp(ready_queue, &queue_cnt, timestamp);
+
+  print_queue("Q2 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+
+  memset(ready_queue, 0, sizeof ready_queue);
+  queue_cnt = 0;
+  timestamp = 8;
+
+  set_pcb(&b1, 6, 3, 4, 0, 0, 4, 5);
+  set_pcb(&b2, 7, 4, 5, 0, 0, 5, 7);
+  set_pcb(&b3, 8, 5, 6, 0, 0, 6, 3);
+  set_pcb(&b4, 9, 7, 5, 0, 0, 5, 9);
+
+  ready_queue[queue_cnt++] = b1;
+  ready_queue[queue_cnt++] = b2;
+  ready_queue[queue_cnt++] = b3;
+  ready_queue[queue_cnt++] = b4;
+
+  returned_pcb =
+      handle_process_completion_pp(ready_queue, &queue_cnt, timestamp);
+
+  print_queue("Q3 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+}
+
+void handle_process_arrival_srtp_test() {
+  struct PCB ready_queue[QUEUEMAX];
+  struct PCB b1, current_process, new_process, returned_pcb;
+  int queue_cnt = 0, timestamp = 2;
+  set_pcb(&current_process, 1, 1, 8, 1, 9, 8, 0);
+  set_pcb(&new_process, 2, 2, 6, 0, 0, 6, 0);
+
+  returned_pcb = handle_process_arrival_srtp(
+      ready_queue, &queue_cnt, current_process, new_process, timestamp);
+
+  print_queue("Q1 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+
+  memset(ready_queue, 0, sizeof ready_queue);
+  queue_cnt = 0;
+  timestamp = 2;
+
+  set_pcb(&current_process, 0, 0, 0, 0, 0, 0, 0);
+  set_pcb(&new_process, 5, 2, 5, 0, 0, 5, 4);
+
+  returned_pcb = handle_process_arrival_srtp(
+      ready_queue, &queue_cnt, current_process, new_process, timestamp);
+
+  print_queue("Q2 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+
+  memset(ready_queue, 0, sizeof ready_queue);
+  queue_cnt = 0;
+  timestamp = 4;
+
+  set_pcb(&current_process, 5, 2, 5, 2, 7, 3, 4);
+  set_pcb(&new_process, 6, 4, 7, 0, 0, 7, 2);
+
+  returned_pcb = handle_process_arrival_srtp(
+      ready_queue, &queue_cnt, current_process, new_process, timestamp);
+
+  print_queue("Q3 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+}
+
+void handle_process_completion_srtp_test() {
+  struct PCB ready_queue[QUEUEMAX];
+  struct PCB b1, b2, b3, b4, returned_pcb;
+
+  int queue_cnt = 0;
+  int timestamp = 2;
+
+  set_pcb(&b1, 1, 1, 23, 0, 0, 23, 0);
+  set_pcb(&b2, 2, 1, 22, 0, 0, 22, 0);
+  set_pcb(&b3, 3, 1, 24, 0, 0, 24, 0);
+
+  ready_queue[queue_cnt++] = b1;
+  ready_queue[queue_cnt++] = b2;
+  ready_queue[queue_cnt++] = b3;
+
+  returned_pcb =
+      handle_process_completion_srtp(ready_queue, &queue_cnt, timestamp);
+
+  print_queue("Q1 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+
+  memset(ready_queue, 0, sizeof ready_queue);
+  queue_cnt = 0;
+  timestamp = 3;
+
+  returned_pcb =
+      handle_process_completion_srtp(ready_queue, &queue_cnt, timestamp);
+
+  print_queue("Q2 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+
+  memset(ready_queue, 0, sizeof ready_queue);
+  queue_cnt = 0;
+  timestamp = 8;
+
+  set_pcb(&b1, 6, 3, 4, 0, 0, 4, 5);
+  set_pcb(&b2, 7, 4, 5, 0, 0, 5, 7);
+  set_pcb(&b3, 8, 5, 6, 0, 0, 6, 3);
+  set_pcb(&b4, 9, 7, 5, 0, 0, 5, 9);
+
+  ready_queue[queue_cnt++] = b1;
+  ready_queue[queue_cnt++] = b2;
+  ready_queue[queue_cnt++] = b3;
+  ready_queue[queue_cnt++] = b4;
+
+  returned_pcb =
+      handle_process_completion_srtp(ready_queue, &queue_cnt, timestamp);
+
+  print_queue("Q3 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+}
+
+void handle_process_arrival_rr_test() {
+  struct PCB ready_queue[QUEUEMAX];
+  struct PCB b1, current_process, new_process, returned_pcb;
+  int queue_cnt = 0, timestamp = 2, time_quantum = 6;
+
+  set_pcb(&current_process, 1, 1, 8, 1, 9, 8, 0);
+  set_pcb(&new_process, 2, 2, 8, 0, 0, 8, 0);
+
+  returned_pcb =
+      handle_process_arrival_rr(ready_queue, &queue_cnt, current_process,
+                                new_process, timestamp, time_quantum);
+
+  print_queue("Q1 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+
+  memset(ready_queue, 0, sizeof ready_queue);
+  queue_cnt = 0;
+  timestamp = 2;
+  time_quantum = 6;
+
+  set_pcb(&current_process, 0, 0, 0, 0, 0, 0, 0);
+  set_pcb(&new_process, 5, 2, 5, 0, 0, 5, 4);
+
+  returned_pcb =
+      handle_process_arrival_rr(ready_queue, &queue_cnt, current_process,
+                                new_process, timestamp, time_quantum);
+
+  print_queue("Q2 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+
+  memset(ready_queue, 0, sizeof ready_queue);
+  queue_cnt = 0;
+  timestamp = 4;
+  time_quantum = 6;
+
+  set_pcb(&current_process, 5, 2, 5, 2, 7, 3, 4);
+  set_pcb(&new_process, 6, 4, 7, 0, 0, 7, 2);
+
+  returned_pcb =
+      handle_process_arrival_rr(ready_queue, &queue_cnt, current_process,
+                                new_process, timestamp, time_quantum);
+
+  print_queue("Q3 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+}
+
+void handle_process_completion_rr_test() {
+  struct PCB ready_queue[QUEUEMAX];
+  struct PCB b1, b2, b3, b4, returned_pcb;
+
+  int queue_cnt = 0;
+  int timestamp = 24;
+  int time_quantum = 10;
+
+  set_pcb(&b1, 1, 22, 8, 0, 0, 8, 0);
+  set_pcb(&b2, 2, 21, 8, 0, 0, 8, 0);
+  set_pcb(&b3, 3, 23, 8, 0, 0, 8, 0);
+
+  ready_queue[queue_cnt++] = b1;
+  ready_queue[queue_cnt++] = b2;
+  ready_queue[queue_cnt++] = b3;
+
+  returned_pcb = handle_process_completion_rr(ready_queue, &queue_cnt,
+                                              timestamp, time_quantum);
+
+  print_queue("Q1 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+
+  memset(ready_queue, 0, sizeof ready_queue);
+  queue_cnt = 0;
+  timestamp = 2;
+  time_quantum = 6;
+
+  returned_pcb = handle_process_completion_rr(ready_queue, &queue_cnt,
+                                              timestamp, time_quantum);
+
+  print_queue("Q2 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+
+  memset(ready_queue, 0, sizeof ready_queue);
+  queue_cnt = 0;
+  timestamp = 12;
+  time_quantum = 6;
+
+  set_pcb(&b1, 6, 3, 4, 0, 0, 4, 5);
+  set_pcb(&b2, 7, 4, 5, 0, 0, 5, 7);
+  set_pcb(&b3, 8, 5, 6, 0, 0, 6, 3);
+  set_pcb(&b4, 9, 7, 5, 0, 0, 5, 9);
+
+  ready_queue[queue_cnt++] = b1;
+  ready_queue[queue_cnt++] = b2;
+  ready_queue[queue_cnt++] = b3;
+  ready_queue[queue_cnt++] = b4;
+
+  returned_pcb = handle_process_completion_rr(ready_queue, &queue_cnt,
+                                              timestamp, time_quantum);
+
+  print_queue("Q3 ", ready_queue, queue_cnt);
+  print_pcb("Returned ", returned_pcb);
+  puts("----------------------------------------------------------");
+}
+int main() {
+
+  // handle_process_arrival_pp_test();
+  // handle_process_completion_pp_test();
+  // handle_process_arrival_srtp_test();
+  // handle_process_completion_srtp_test();
+  // handle_process_arrival_rr_test();
+  handle_process_completion_rr_test();
+  return 0;
+}
